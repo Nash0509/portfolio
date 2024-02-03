1f07535fc847e8133df0aa81f5510ea7038b6490
unknown <nishantsinghworkshard@gmail.com>
2024-02-03 23:49:42 +0530

    Eslint problem solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,13 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Skills from './Skills';
-import { motion, useScroll } from "framer-motion";
 
 
 const Navbar = () => {
-  const { scrollYProgress } = useScroll();
   return (
    <div>
      <div className='grid grid-cols-2 gap-2 p-5'>
-      <div><Link href='/' className='font-bold lg:text-3xl sm:text-xl'>NISHANT's portfolio</Link></div>
+      <div><Link href='/' className='font-bold lg:text-3xl sm:text-xl'>NISHANT portfolio</Link></div>
       <div>
         <ul className='grid grid-cols-4 gap-1'>
           <Link href='#home' ><li>Home</li></Link>
@@ -24,10 +22,6 @@ const Navbar = () => {
         </ul>
       </div>
     </div>
-    <motion.div
-        className="progress-bar"
-        style={{ scaleX: scrollYProgress }}
-      />
    <div>
    <Home/>
    <Skills/>
